ccf968aa3fcd0fe77effbac7bed3087b3339e501
TatsuyaOkabe <okabetatsuyaof@gmail.com>
2020-08-03 07:44:59 +0900

    [add]　ソースをインクルード

--- MainGameProject/GameSources/Project.h
+++ MainGameProject/GameSources/Project.h
@@ -8,7 +8,12 @@
 
 #include "ProjectShader.h"
 #include "ProjectBehavior.h"
+#include "StructData.h"
 #include "Scene.h"
+
+#include "ObjectBase.h"
+#include "UIBase.h"
+
 #include "GameStage.h"
 #include "Character.h"
 #include "Player.h"
