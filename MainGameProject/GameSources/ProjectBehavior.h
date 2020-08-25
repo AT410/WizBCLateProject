890efa17c02ad9a026d2b03c8e55a44585f2934b
TatsuyaOkabe <okabetatsuyaof@gmail.com>
2020-08-25 09:45:44 +0900

    [add]　ハンドラの追加

--- MainGameProject/GameSources/ProjectBehavior.h
+++ MainGameProject/GameSources/ProjectBehavior.h
@@ -7,7 +7,17 @@
 #include "stdafx.h"
 
 namespace basecross {
-
+	template<typename T>
+	struct InputHandler {
+		void PushHandle(const shared_ptr<T>& Obj) {
+			auto cntlVec = App::GetApp()->GetInputDevice().GetControlerVec();
+			if (cntlVec[0].bConnected) {
+				if (cntlVec[0].wPressedButtons & XINPUT_GAMEPAD_A) {
+					Obj->PushA();
+				}
+			}
+		}
+	};
 }
 
 //end basecross
