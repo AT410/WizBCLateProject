62178c917270a403eddabd14defc17179ceda96e
TatsuyaOkabe <okabetatsuyaof@gmail.com>
2020-09-02 09:35:18 +0900

    [add]　描画用のキャラクター作成

--- MainGameProject/GameSources/Character.cpp
+++ MainGameProject/GameSources/Character.cpp
@@ -7,7 +7,33 @@
 #include "Project.h"
 
 namespace basecross{
+	Character::Character(const shared_ptr<Stage>& stage,
+		Vec3& rotation,
+		Vec3& scale,
+		Vec3& position,
+		int& Layer,
+		wstring& Texture):
+	ObjectBase(stage, rotation, scale, position, Layer, Texture)
+	{}
 
+	Character::Character(const shared_ptr<Stage>& stage,
+		ObjectData& objectdata):
+	ObjectBase(stage, objectdata)
+	{} 
+
+	void Character::OnCreate() {
+		DrawingImage(m_textureName);
+		InitializeTransfrom();
+	}
+
+	void Character::DestroyCharacter() {
+		SetDrawActive(false);
+	}
+
+	void Character::MoveCharacter(Vec3 pos) {
+		auto ptrTransform = GetComponent<Transform>();
+		ptrTransform->SetPosition(pos);
+	}
 
 }
 //end basecross
--- MainGameProject/GameSources/Character.h
+++ MainGameProject/GameSources/Character.h
@@ -7,7 +7,25 @@
 #include "stdafx.h"
 
 namespace basecross{
+	class Character : public ObjectBase {
 
+	public:
+		Character(const shared_ptr<Stage>& stage,
+			Vec3& rotation,
+			Vec3& scale,
+			Vec3& position,
+			int& Layer,
+			wstring& Texture);
+
+		Character(const shared_ptr<Stage>& stage,
+			ObjectData& objectdata);
+
+		virtual void OnCreate() override;
+
+		void DestroyCharacter();
+
+		void MoveCharacter(Vec3 pos);
+	};
 
 }
 //end basecross
